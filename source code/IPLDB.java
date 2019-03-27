489937a6ee3264dc09966a858d60ee064c86ddc6
Sharath Dev <s>
2019-03-27 16:23:00 +0530

    no message

--- source code/IPLDB.java
+++ source code/IPLDB.java
@@ -5,28 +5,62 @@ import java.sql.*;
 
 class IPLDB {
     JFrame MainFrame;
-    JTextField PlayerNameTextField, TeamNameTextField;
-    JLabel PlayerNameLabel, TeamNameLabel;
+    JPanel EnterPanel, RadioPanel;
+    JTextField NameTextField;
+    JLabel NameLabel;
     JButton EnterButton;
+    JRadioButton PlayerRadioButton, TeamRadioButton;
+    ButtonGroup RadioGroup;
 
-    void ipldb() {
+    boolean GetRadioButtonState()
+    {
+        return PlayerRadioButton.isSelected()? true : false;
+    }
+
+    void Display() {
         MainFrame = new JFrame("IPL Database");
-        PlayerNameLabel = new JLabel("Player Name");
-        PlayerNameTextField = new JTextField(10);
-        TeamNameLabel = new JLabel("Team");
-        TeamNameTextField = new JTextField(10);
+
+        EnterPanel = new JPanel();
+        NameLabel = new JLabel("Player Name");
+        NameTextField = new JTextField(10);
         EnterButton = new JButton("Enter");
-        MainFrame.add(PlayerNameLabel); MainFrame.add(PlayerNameTextField);
-        MainFrame.add(TeamNameLabel); MainFrame.add(TeamNameTextField);
+        EnterPanel.add(NameLabel);
+        EnterPanel.add(NameTextField);
+
+        PlayerRadioButton = new JRadioButton("Player Name");
+        TeamRadioButton = new JRadioButton("Team Name");
+        RadioGroup = new ButtonGroup();
+        RadioGroup.add(PlayerRadioButton);
+        RadioGroup.add(TeamRadioButton);
+
+        RadioPanel = new JPanel();
+        RadioPanel.add(PlayerRadioButton);
+        RadioPanel.add(TeamRadioButton);
+        MainFrame.add(EnterPanel);
+        MainFrame.add(RadioPanel);
+
+        /*MainFrame.add(NameLabel); MainFrame.add(NameTextField);
+        MainFrame.add(PlayerRadioButton); MainFrame.add(TeamRadioButton);*/
         MainFrame.add(EnterButton);
+
         MainFrame.setLayout(new FlowLayout());
         MainFrame.setSize(500,500); // JFrame Size
         MainFrame.setVisible(true); //JFrame visibility
-    }
+        MainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        EnterButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent ae)
+            {
+                boolean state = GetRadioButtonState();
+                String text = NameTextField.getText();
+            }
+        });
+    }
 
     public static void main(String args[])
     {
-        new IPLDB();
+        IPLDB kek = new IPLDB();
+        kek.Display();
     }
 }
